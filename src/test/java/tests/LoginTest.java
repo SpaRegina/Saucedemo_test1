ecb92fcbf25dcaf3d536103d465acedfef32adc3
SpaRegina <r.g.spaschenko@gmail.com>
2025-09-28 14:31:09 +0500

    Доработаны: тест с параметризацей, тест на добавление товаров в корзину (добавлен CartPage). Изменен pom.xml файл.

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import tests.parent.BaseTest;
 
@@ -14,32 +15,26 @@ import static org.testng.Assert.*;
 public class LoginTest extends BaseTest {
 
     @Test(description = "Проверка корректной авторизации")
-    public void testSuccessfulLogin() {
+    public void checkCorrectLogin() throws InterruptedException {
         loginPage.open();
-        loginPage.login("standard_user", "secret_sauce");
+        loginPage.loginSwagLabs("standard_user", "secret_sauce");
         assertTrue(productsPage.isTitlePresent());
+        assertEquals(productsPage.getTitle(), "Products", "Название заголовка не соответствует ожидаемому.");
     }
 
-    @Test(description = "Проверка авторизации с заблокированным пользователем")
-    public void testLockedOutUserError() {
-        loginPage.open();
-        loginPage.login("locked_out_user", "secret_sauce");
-        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Sorry, this user has been locked out.");
+    @DataProvider()
+    public Object[][] loginData() {
+        return new Object[][] {
+                {"locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."},
+                {"", "secret_sauce", "Epic sadface: Username is required"},
+                {"standard_user", "", "Epic sadface: Password is required"}
+        };
     }
 
-    @Test(description = "Проверка ошибки при пустом имени пользователя")
-    public void testLoginWithEmptyUsername() {
+    @Test(dataProvider = "loginData")
+    public void checkIncorrectLogin(String user, String rassword, String errorMsg) {
         loginPage.open();
-        loginPage.login("", "secret_sauce");
-        String expectedError = "Epic sadface: Username is required";
-        assertEquals(loginPage.checkErrorMsg(), expectedError, "Текст ошибки для пустого имени пользователя неверный.");
-    }
-
-    @Test(description = "Проверка ошибки при пустом пароле")
-    public void testLoginWithEmptyPassword() {
-        loginPage.open();
-        loginPage.login("standard_user", "");
-        String expectedError = "Epic sadface: Password is required";
-        assertEquals(loginPage.checkErrorMsg(), expectedError, "Текст ошибки для пустого пароля неверный.");
+        loginPage.loginSwagLabs(user, rassword);
+        assertEquals(loginPage.checkErrorMsg(), errorMsg);
     }
 }
